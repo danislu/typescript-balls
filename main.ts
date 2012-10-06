9e2a0739c951c4398eaef64867ae5450e3625337
daniel <danislu@gmail.com>
2012-10-06 23:25:36 +0200

    2nd

--- main.ts
+++ main.ts
@@ -4,11 +4,12 @@ module Dsl {
 
         collection: BallCollection;
 
-        constructor (public height : number, public width : number, public context, ballCount: number = 1) {
-            if (ballCount < 1) ballCount = 1;
-            var i,
+        constructor (public height : number, public width : number, public xOffset : number, public yOffset : number, public context, ballCount: number = 1) {
+            var i : number,
                 ball : Ball,
-                balls : Ball[];
+                balls : Ball[] = new Ball[];
+
+            if (ballCount < 1) ballCount = 1;
 
             for (i = 0; i < ballCount; i++) {
                 ball = new Ball(context, new Point(Math.random() * width, Math.random() * height, 0));
@@ -17,11 +18,77 @@ module Dsl {
            
             this.collection = new BallCollection();
             this.collection.balls = balls;
+
+            function getHitPoint(e: MouseEvent) : Point {
+                return new Point(e.pageX - xOffset, e.pageY - yOffset);
+            }
+
+            window.onmousedown = (e) => {
+                var hitPoint = getHitPoint(e),
+                    prop: string,
+                    ball: Ball;
+
+                for (prop in this.collection.balls) {
+                    ball = this.collection.balls[prop];
+                    if (ball.isHit(hitPoint)) {
+                        this.startPoint = hitPoint;
+                        this.currentBall = ball;
+                        break;
+                    }
+                }
+            }
+
+            window.onmousemove = (e) => {
+                if (this.currentBall != null) {
+                    this.currentPoint = getHitPoint(e);
+                }
+            }
+
+            window.onmouseup = (e) => {
+                if (this.currentBall != null && this.startPoint != null) {
+                    this.currentPoint = getHitPoint(e);
+                         
+                    this.currentBall.velocity = getVector(this.startPoint, this.currentPoint);
+                }
+                this.currentBall = null;
+                this.startPoint = null;
+            }
         }
 
-        tick() {
+        private currentBall: Ball;
+        private startPoint: Point;
+        private currentPoint: Point;
+        private running: bool = false;
+        public start() {
+            this.running = true;
+            this.tick();
+        }
+
+        public stop() {
+            this.running = false;
+        }
+
+        private tick() {
             this.collection.update();
             this.collection.draw();
+            
+            this.draw();
+
+            if (this.running) {
+                setTimeout(() => {
+                    this.tick()
+                }, 50);
+            }
+        }
+
+        private draw() {
+            if (this.startPoint && this.currentPoint) {
+                this.context.beginPath();
+                this.context.moveTo(this.startPoint.x, this.startPoint.y);
+                this.context.lineTo(this.currentPoint.x, this.currentPoint.y);
+                this.context.closePath();
+                this.context.stroke();
+            }
         }
     }
 
@@ -29,11 +96,9 @@ module Dsl {
         balls: Ball[];
 
         draw() {
-
             var ball: Ball,
                 prop: string;
 
-            // update all balls position
             for (prop in this.balls) {
                 ball = this.balls[prop];
                 ball.draw();
@@ -71,12 +136,17 @@ module Dsl {
 
     class Ball {
         touched: bool;
-        velocity: Vector;
+        velocity: Vector = new Vector(0,0);
         
-        constructor (public context, public location: Point, public size: number = 10, public colour: string = "black") { }
+        constructor (public context, public location: Point, public size: number = 10, public colour: string = "#00000") { }
 
         draw() {
-            //this.context
+            if (this.touched)
+                this.context.fillStyle = "#fffff";
+            else
+                this.context.fillStyle = this.colour;
+            this.context.arc(this.location.x, this.location.y, this.size, 0, 360, false);
+            this.context.fill();
         }
 
         update(steps: number = 1) {
@@ -88,34 +158,48 @@ module Dsl {
                 this.location.y += this.velocity.y;
                 this.location.z += this.velocity.z;
 
-                this.velocity.update(0.1);
+                this.velocity.addFriction(0.1);
             }
         }
 
         isCrashed(otherBall: Ball): bool {
-            var x = Math.abs(this.location.x - otherBall.location.x);
-            var y = Math.abs(this.location.y - otherBall.location.y);
-            var z = Math.abs(this.location.z - otherBall.location.z);
-
-            var dist = Math.sqrt(x * x + y * y + z * z)
-
+            var dist = this.location.getDist(otherBall.location);
             return dist <= 20;
         }
+
+        isHit(hit: Point) : bool {
+            var dist = this.location.getDist(hit);
+            return dist <= this.size;
+        }
+
+        
     }
 
     class Point {
-        constructor (public x: number, public y: number, public z: number) { }
+        constructor (public x: number, public y: number, public z: number = 0) { }
+
+        getDist(other: Point) : number {
+            var x = Math.abs(this.x - other.x);
+            var y = Math.abs(this.y - other.y);
+            var z = Math.abs(this.z - other.z);
+
+            return Math.sqrt(x * x + y * y + z * z);
+        }
+    }
+
+    function getVector(a: Point, b: Point): Vector {
+        return new Vector(Math.abs(a.x - b.x), Math.abs(a.y - b.y), Math.abs(a.z - b.z));
     }
 
     class Vector extends Point {
-        constructor (x: number, y: number, z: number) {
+        constructor (x: number, y: number, z: number = 0) {
             super(x, y, z);
         }
-        update(friction: number) {
+
+        addFriction(friction: number) {
             this.x *= friction;
             this.y *= friction;
             this.z *= friction;
         }
     }
-
 }
